d185ec736519d4d6e8dfb8a5a015886f2124dac3
Muhammad Ali Bhatti <60929796+Ali-Bhatti@users.noreply.github.com>
2025-09-02 18:23:38 +0500

    chore: stub axios to unblock demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { FGDesignDemoRoutes } from './FGDesignDemo/routes';
+import { componentRegistry } from './FGDesignDemo/componentRegistry';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {FGDesignDemoRoutes}
-        <Route path="*" element={<Navigate to="/fg-demo/buttons" replace />} />
+        <Route path="*" element={<Navigate to={componentRegistry[0].path} replace />} />
       </Routes>
     </BrowserRouter>
   );
--- src/FGDesignDemo/routes.tsx
+++ src/FGDesignDemo/routes.tsx
@@ -5,12 +5,8 @@ import { componentRegistry } from './componentRegistry';
 export const FGDesignDemoRoutes = (
   <Route path="/fg-demo" element={<AppShell />}>
     {componentRegistry.map((item) => (
-      <Route
-        key={item.key}
-        path={item.path.replace('/fg-demo/', '')}
-        element={item.element}
-      />
+      <Route key={item.key} path={item.key} element={item.element} />
     ))}
-    <Route index element={<Navigate to={componentRegistry[0].path.replace('/fg-demo/', '')} replace />} />
+    <Route index element={<Navigate to={componentRegistry[0].key} replace />} />
   </Route>
 );
